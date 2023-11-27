332e62176d57315131d99d6dc8f40b3a65730ed5
mitchp819 <135446256+mitchp819@users.noreply.github.com>
2023-11-27 13:30:59 -0500

    work on JSON request

--- eventSearch/views.py
+++ eventSearch/views.py
@@ -1,37 +1,40 @@
 from django.shortcuts import render
+from datetime import datetime
 import requests
 
-#response = requests.get("https://app.ticketmaster.com/discovery/v2/events.json?size=20&apikey=Ij0EiPF3QAJv6NvdHh92ALlIHgMK8a7U&city=${city}&keyword=${classificationName}&sort=date,asc")
+#my javascript json response -> response = requests.get("https://app.ticketmaster.com/discovery/v2/events.json?size=20&apikey=Ij0EiPF3QAJv6NvdHh92ALlIHgMK8a7U&city=${city}&keyword=${classificationName}&sort=date,asc")
 
 # Create your views here.
 
-def event_search(request):
-    get_events("hartford","music")
-    return render(request, 'event-search.html')
+#card info holds data to be displayed on each card
+card_info=[]
 
+#Gets events through JSON request then renders card
+def event_search(request):
+
+    get_events("hartford","music")
+    return render(request, 'event-search.html', context={'cards': card_info})
+
+#JSON request -> event_search_format
 def get_events(city, classification_name):
     try:
-        response = requests.get("https://app.ticketmaster.com/discovery/v2/events.json?size=20&apikey=Ij0EiPF3QAJv6NvdHh92ALlIHgMK8a7U&city=${city}&keyword=${classification_name}&sort=date,asc")
-
-        """
-        url = "https://app.ticketmaster.com/discovery/v2/events.json?size=20&apikey=Ij0EiPF3QAJv6NvdHh92ALlIHgMK8a7U"
-        parameters = {
-            "size": = 20,
-            "api_key":= Ij0EiPF3QAJv6NvdHh92ALlIHgMK8a7U,
-            "city": = city,
-            "keyword": = classification_name,
-            "sort": "date,asc",
-        }
-        """
+        response = requests.get("https://app.ticketmaster.com/discovery/v2/events.json?size=20&apikey=Ij0EiPF3QAJv6NvdHh92ALlIHgMK8a7U&city=" + city + "&keyword=" + classification_name +"&sort=date,asc")
 
         #send GET request to URL w/ params
         #response = requests.get(url, params=parameters)
         #Raise exception for 4xx and 5xx status code
         response.raise_for_status()
         data = response.json()
-        print(data)
+        #print(data)
+        event_search_format(data)
         return data
     except requests.exceptions.RequestException as e:
         print(f"Request failed: {e}")
         return None
 
+#using JSON data create data for cards
+def event_search_format(data):
+    total_elements = data["page"]["totalElements"]
+    total_results = data["page"]["size"]
+    eventsPath = data["_embedded"]["events"]
+    print(eventsPath)
