fadb90421243cc2bacf339696323d3521f4c8639
mitchp819 <135446256+mitchp819@users.noreply.github.com>
2023-11-29 12:53:34 -0500

    working better

--- eventSearch/views.py
+++ eventSearch/views.py
@@ -2,60 +2,65 @@ from django.shortcuts import render
 from datetime import datetime
 import requests
 
-#my javascript json response -> response = requests.get("https://app.ticketmaster.com/discovery/v2/events.json?size=20&apikey=Ij0EiPF3QAJv6NvdHh92ALlIHgMK8a7U&city=${city}&keyword=${classificationName}&sort=date,asc")
+# my javascript json response -> response = requests.get("https://app.ticketmaster.com/discovery/v2/events.json?size=20&apikey=Ij0EiPF3QAJv6NvdHh92ALlIHgMK8a7U&city=${city}&keyword=${classificationName}&sort=date,asc")
 
 # Create your views here.
 
-#card info holds data to be displayed on each card
-card_info=[]
+# card info holds data to be displayed on each card
+card_info = []
 
-#Gets events through JSON request then renders card
+
+# Gets events through JSON request then renders card
 def event_search(request):
-
-    get_events("hartford","music")
+    get_events("hartford", "music")
+    print(card_info)
     return render(request, 'event-search.html', context={'cards': card_info})
 
-#JSON request -> event_search_format
+
+# JSON request -> event_search_format
 def get_events(city, classification_name):
     try:
-        response = requests.get("https://app.ticketmaster.com/discovery/v2/events.json?size=20&apikey=Ij0EiPF3QAJv6NvdHh92ALlIHgMK8a7U&city=" + city + "&keyword=" + classification_name +"&sort=date,asc")
+        response = requests.get(
+            "https://app.ticketmaster.com/discovery/v2/events.json?size=20&apikey=Ij0EiPF3QAJv6NvdHh92ALlIHgMK8a7U&city=" + city + "&keyword=" + classification_name + "&sort=date,asc")
 
-        #send GET request to URL w/ params
-        #response = requests.get(url, params=parameters)
-        #Raise exception for 4xx and 5xx status code
+        # send GET request to URL w/ params
+        # response = requests.get(url, params=parameters)
+        # Raise exception for 4xx and 5xx status code
         response.raise_for_status()
         data = response.json()
-        #print(data)
+        # print(data)
         event_search_format(data)
         return data
     except requests.exceptions.RequestException as e:
         print(f"Request failed: {e}")
         return None
 
-#using JSON data create data for cards
+
+# using JSON data create data for cards
 def event_search_format(data):
     total_elements = data["page"]["totalElements"]
     total_results = data["page"]["size"]
     eventsPath = data["_embedded"]["events"]
     for item in eventsPath:
         name = item["name"]
+        venue_path = item["_embedded"]["venues"][0]
+        venue_city = venue_path["city"]["name"]
+        venue_state = venue_path["state"]["name"]
+        venue_address = venue_path["address"]["line1"]
+
+        print(venue_city)
 
         best_image = item["images"][0]
-        print(best_image)
         for image in item["images"]:
             this_image = image
             if (this_image["width"] > best_image["width"]):
                 best_image = this_image
         image = best_image["url"]
 
-        #need to format into date and time
-        #dateTime = item["dates"]["start"]["dateTime"]
+        # need to format into date and time
+        # dateTime = item["dates"]["start"]["dateTime"]
 
-        #venue_path = item["_embedded"]["venues"]
-        #venue_city = item["_embedded"]["venues"]["city"]["name"]
-        #venue_state = item["_embedded"]["venues"]["state"]["name"]
-        #venue_address_l1 = item["_embedded"]["venues"]["address"]["line1"]
         venue_ticket_link = item["url"]
 
-        event_data = [name,image,venue_ticket_link]
+        event_data = [name, image, venue_ticket_link]
         card_info.append(event_data)
